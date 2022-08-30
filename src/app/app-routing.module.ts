3fba0f0d96c048343dfc041368405c96ba8cc34e
miguel <miguel.arellano@jaak.ai>
2022-08-29 20:28:42 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo:'miguel-alejandro'
+  },
+
+  {
+    path: 'miguel-alejandro',
+    component: HomeComponent
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo:'miguel-alejandro'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
